8cf3dbfa8a3510120aa9bd9c42261eacc5b55ec5
pns99 <pnsantosh99@gmail.com>
2023-01-19 06:56:28 +0000

    relase v1.0

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement Santu Lamda Jenkins
+    # TODO implement Santu Lamda Jenkins and create release v1.0
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Santosh from Lambda!')
+        'body': json.dumps('Hello Santosh from Lambda!:v1.0')
     }
